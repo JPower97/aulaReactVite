af03a3bd7668c8fcf69984a7c28b075bbed1cf6a
JPower97 <jefferson.sggomes@gmail.com>
2024-06-28 16:17:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+
+  return (
+    <div className="container">
+      <h2>Cálculo de média</h2>
+      <Card/>
+    </div>
+   
+  )
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+export default function Button() {
+
+    return (
+      <div className="button">
+        <button>Calcular</button>
+      </div>
+    )
+  
+  }
